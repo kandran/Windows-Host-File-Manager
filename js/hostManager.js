5060ff953655478c0e11a27ff2a6d0604afd5640
SAEZ Jonathan <saez.jonathan@gmail.com>
2014-10-16 16:06:06 +0200

    design + localstorage + edit host line

--- js/hostManager.js
+++ js/hostManager.js
@@ -7,107 +7,129 @@ var readline = require('readline');
 //host config
 var filename;
 var lineNumber =0;
-var hostFile = Array();
+var hostFile = Array()
+var oldLine = '';
 
 init();
 
 
 // App config
-function init(){
-    detectPlatform();
-    filename = localStorage.getItem('fileName');
-    if(filename === null){
-        filename = 'C:\\WINDOWS\\system32\\drivers\\etc\\hosts';
-        localStorage.setItem('fileName', filename);
-    }
+    function init(){
+        detectPlatform();
+        filename = localStorage.getItem('fileName');
+        if(filename === null){
+            filename = 'C:\\WINDOWS\\system32\\drivers\\etc\\hosts';
+            localStorage.setItem('fileName', filename);
+        }
 
 
-}
-function detectPlatform()
-{
-    if (process.platform !== 'win32') {
-        alert('Le systeme ne fonctionne que sur windows');
-        gui.App.quit();
     }
-}
+    function detectPlatform()
+    {
+        if (process.platform !== 'win32') {
+            alert('Le systeme ne fonctionne que sur windows');
+            gui.App.quit();
+        }
+    }
 
 
 
 // Get data from host file and show
 
-function readHostFile(){
+    function readHostFile(){
 
-    document.getElementById('viewsHost').innerHTML="";
+        document.getElementById('viewsHost').innerHTML="";
 
-    lineNumber =0;
-    var rd = readline.createInterface({
-        input: fs.createReadStream(filename),
-        output: process.stdout,
-        terminal: false
-    });
+        lineNumber =0;
+        var rd = readline.createInterface({
+            input: fs.createReadStream(filename),
+            output: process.stdout,
+            terminal: false
+        });
 
-    rd.on('line', function(line) {
-        hostFile.push(line);
-        console.log('Read host file');
+        rd.on('line', function(line) {
+            hostFile.push(line);
+            console.log('Read host file');
 
-    }).on('close', function() {
-        for(line in hostFile){
-            showHostLine(hostFile[line]);
+        }).on('close', function() {
+            for(line in hostFile){
+                showHostLine(hostFile[line]);
+            }
+        });
+
+
+    }
+
+    function showHostLine(line){
+        host = document.getElementById('viewsHost');
+        Expression = new RegExp("^#","i");
+
+        if(line != "" && line !="#"){
+            if(Expression.test(line)){
+                state = 'comment';
+            }else{
+                state = 'uncomment';
+            }
+            hostLine = '<p id="host-'+ lineNumber +'" class="hostLine ' + state +'">' + '<input type="text" value="'+line + '" onclick="changeVisibilitySave(\'host-'+lineNumber+'\')"'  + '<span class="control">'+ createButton('save', state, lineNumber) +createButton('change_status',state, lineNumber) + createButton('delete', state, lineNumber)+  ' </span><p>';
+            host.innerHTML += hostLine.replace(/\r\n/g, '<br class="retour" />').replace(/[\r\n]/g, '<br  class="retour" />');
         }
-    });
+        lineNumber++;
+    }
 
-
-}
-
-function showHostLine(line){
-    host = document.getElementById('viewsHost');
-    Expression = new RegExp("^#","i");
-
-    if(line != "" && line !="#"){
-        if(Expression.test(line)){
-            state = 'comment';
+    function createChangeStatusButton(context) {
+        var button;
+        if(context == 'comment'){
+            button =  '<img src="media/fi-italic.svg" alt="Uncomment" onclick="Uncomment(\'host-'+lineNumber+'\')">';
         }else{
-            state = 'uncomment';
+            button =  '<img src="media/fi-italic.svg" alt="Comment" onclick="Comment(\'host-'+lineNumber + '\')">';
         }
-        hostLine = '<p id="host-'+ lineNumber +'" class="hostLine ' + state +'">' + '<input type="text" value="'+line + '">'  + '<span class="control">'+ createButton('change_status',state, lineNumber) + createButton('delete', state, lineNumber)+  ' </span><p>';
-        host.innerHTML += hostLine.replace(/\r\n/g, '<br class="retour" />').replace(/[\r\n]/g, '<br  class="retour" />');
+        return button;
     }
-    lineNumber++;
-}
 
-function createChangeStatusButton(context, lineNumber) {
-    var button;
-    if(context == 'comment'){
-         button =  '<img src="media/fi-italic.svg" alt="Uncomment" onclick="Uncomment(\'host-'+lineNumber+'\')">';
-        }else{
-        button =  '<img src="media/fi-italic.svg" alt="Comment" onclick="Comment(\'host-'+lineNumber + '\')">';
+
+    function createButton(type, context, lineNumber)
+    {
+        switch (type){
+            case 'change_status' :
+                return  createChangeStatusButton(context, lineNumber);
+                break;
+            case 'delete' :
+                return  createDeleteButton(context);
+                break;
+            case 'save' :
+                return createSaveButton(context, lineNumber);
+            default :
+        }
+        return '';
     }
-    return button;
-}
 
-function createButton(type, context, lineNumber)
-{
-    switch (type){
-        case 'change_status' :
-            return  createChangeStatusButton(context, lineNumber);
-            break;
-        case 'delete' :
-            return  createDeleteButton(context, lineNumber);
-            break;
-        default :
+    function createDeleteButton(context){
+        return  '<img src="media/fi-x.svg" alt="Delete" onclick="Delete(\'host-'+ lineNumber+'\')">';
     }
-    return '';
+
+    function createSaveButton(context){
+        return  '<img src="media/fi-save.svg" alt="Save" id="Save-host-'+ lineNumber +'" onclick="Save(\'host-'+ lineNumber+'\')" class="hidden">';
+
 }
 
-function createDeleteButton(context){
-    return  '<img src="media/fi-x.svg" alt="Delete" onclick="Delete(\'host-'+ lineNumber+'\')">';
+function changeVisibilitySave(lineNumber){
+    var idSave= 'Save-'+lineNumber;
+    if(document.getElementById(idSave).className === 'hidden'){
+        document.getElementById(idSave).className='visible';
+        oldLine = document.getElementById(lineNumber).children[0].value;
+    }
 }
-
-
 
 //Modify host file
 
-function action(button, lineNumber){
+    function Save(lineNumber){
+        lineNumberVal = parseInt(lineNumber.substr(5))
+        hostFile[lineNumberVal] = document.getElementById(lineNumber).children[0].value;
+        document.getElementById('Save-'+lineNumber).className='hidden';
+        writeFile();
+    }
+
+    function action(button, lineNumber){
     var fn = window[button.getAttribute('value')];
     if(typeof fn === 'function') {
         fn(lineNumber);
